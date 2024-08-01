617c998571a8c89eedb69459f23215bf6a1d640c
gemaymini <446914334@qq.com>
2024-08-01 18:01:53 +0800

    增加了修改函数

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component{
     addTodoItem=()=>{
       const newTodoItem={
         id:this.state.todoItems.length,
-        value:this.refs.todoItemValue.value,
+        value:this.todoItemValue.value,
         done:false,
         delete:false
       };
@@ -23,33 +23,42 @@ export default class App extends React.Component{
     }
 
     deleteTodoItem=(item)=>{
-      item.delete=true;
-      this.setState(
-        {
-          todoItems:[...this.state.todoItems,item]
-        }
-      )
+      const newitems=this.state.todoItems.map(
+        (todoitem)=>todoitem.id==item.id?{...todoitem,delete:true}:todoitem
+      );
+      this.setState({todoItems:newitems});
     }
 
-    render(){
-      return(
+    
+    updateToDoItem = (item) => {
+      let updatedValue = prompt("Enter new value:", item.value);
+      if (updatedValue !== null) {
+        const newItems = this.state.todoItems.map(
+          (todoItem) => todoItem.id === item.id ? { ...todoItem, value: updatedValue } : todoItem
+        );
+        this.setState({ todoItems: newItems});
+      }
+    };
+  
+    render() {
+      return (
         <div>
-          <h1>My First React App --TODOLIST</h1> 
+          <h1>My First React App --TODOLIST</h1>
           <div>
-            <input type='text' placeholder='add something...' ref='todoItemValue'/>
+            <input type='text' placeholder='add something...' ref={(input) => this.todoItemValue = input} />
             <button type='submit' onClick={this.addTodoItem}>添加</button>
           </div>
           <ul>
             {
-              this.state.todoItems.map((item)=>
-              {
-                if(item.delete) return ;
+              this.state.todoItems.map((item) => {
+                if (item.delete) return null;
                 return (
                   <li key={item.id}>
                     <label>{item.value}</label>
-                    <button onClick={()=>this.deleteTodoItem(item)}>删除</button>
+                    <button onClick={() => this.deleteTodoItem(item)}>删除</button>
+                    <button onClick={() => this.updateToDoItem(item)}>修改</button>
                   </li>
-                )
+                );
               })
             }
           </ul>
